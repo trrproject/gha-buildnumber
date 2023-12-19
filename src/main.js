0a9b86be1d75effcd3a445392aee0f26ce56218a
trrproject <jomgsantos@gmail.com>
2023-12-19 22:22:47 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,103 @@
+//Trying to avoid any npm installs or anything that takes extra time...
+const   https = require('https'),
+        zlib = require('zlib'),
+        fs = require('fs'),
+        env = process.env;
+
+function fail(message, exitCode=1) {
+    console.log(`::error::${message}`);
+    process.exit(1);
+}
+
+function request(method, path, data, callback) {
+    
+    try {
+        if (data) {
+            data = JSON.stringify(data);
+        }  
+        const options = {
+            hostname: 'api.github.com',
+            port: 443,
+            path,
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': data ? data.length : 0,
+                'Accept-Encoding' : 'gzip',
+                'Authorization' : `token ${env.INPUT_TOKEN}`,
+                'User-Agent' : 'GitHub Action - development'
+            }
+        }
+        const req = https.request(options, res => {
+    
+            let chunks = [];
+            res.on('data', d => chunks.push(d));
+            res.on('end', () => {
+                let buffer = Buffer.concat(chunks);
+                if (res.headers['content-encoding'] === 'gzip') {
+                    zlib.gunzip(buffer, (err, decoded) => {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            callback(null, res.statusCode, decoded && JSON.parse(decoded));
+                        }
+                    });
+                } else {
+                    callback(null, res.statusCode, buffer.length > 0 ? JSON.parse(buffer) : null);
+                }
+            });
+    
+            req.on('error', err => callback(err));
+        });
+    
+        if (data) {
+            req.write(data);
+        }
+        req.end();
+    } catch(err) {
+        callback(err);
+    }
+}
+
+
+function main() {
+    
+    //Some sanity checking:
+    for (let varName of ['INPUT_TOKEN', 'GITHUB_REPOSITORY']) {
+        if (!env[varName]) {
+            fail(`ERROR: Environment variable ${varName} is not defined.`);
+        }
+    }
+
+    request('GET', `/repos/${env.GITHUB_REPOSITORY}/git/refs/tags/v`, null, (err, status, result) => {
+    
+        let nextBuildNumber=1;
+    
+        if (status === 404) {
+            console.log('No build-number ref available, starting at 1.');
+        } else if (status === 200) {
+
+            let tag=(result[result.length-1].ref).split('/')[2];
+            let currentBuildNumber=(tag.split('.')[2]).split('-')[0];
+
+            console.log(`Last build nr was ${currentBuildNumber}.`);
+    
+            nextBuildNumber = parseInt(currentBuildNumber) + 1;
+            console.log(`Updating build counter to ${nextBuildNumber}...`);
+        } else {
+            if (err) {
+                fail(`Failed to get refs. Error: ${err}, status: ${status}`);
+            } else {
+                fail(`Getting build-number refs failed with http status ${status}, error: ${JSON.stringify(result)}`);
+            } 
+        }
+
+        fs.writeFileSync(process.env.GITHUB_OUTPUT, `build_number=${nextBuildNumber}`);
+        fs.writeFileSync(process.env.GITHUB_ENV, `BUILD_NUMBER=${nextBuildNumber}`);
+
+    });
+}
+
+main();
+
+
